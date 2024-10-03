1622f21f48f8d7a824d3098b41dcb2c93643104c
Anand Shinde <55067572+AnandShinde02@users.noreply.github.com>
2024-10-03 16:38:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import HeroCarousel from './components/HeroCarousel';
+import AboutUs from './components/AboutUs';
+import Gallery from './components/Gallery';  // Importing the Gallery component
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Other components like HeroSection, AboutUs will go here */}
+      <HeroCarousel />
+      <AboutUs />
+      <Gallery />  {/* Adding the Gallery Section */}
+      {/* Other components like Footer */}
     </div>
   );
 }
